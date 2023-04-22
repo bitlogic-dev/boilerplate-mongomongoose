661f1aea783473007cefa328615a93af8e475233
Haig Hagopian <8030808+bitlogic-dev@users.noreply.github.com>
2023-04-21 18:01:01 -0700

    transfer fcc solutions from repl.it

--- myApp.js
+++ myApp.js
@@ -2,61 +2,146 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-let Person;
+// connect to mongodb cluster
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// create schema
+const personSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+
+// create model
+const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  // create new instance of Person model
+  const person = new Person({
+    name: 'name',
+    age: 45,
+    favoriteFoods: ['']
+  });
+
+  // use built-in mongoose method to save record
+  person.save((err, data) => {
+    if(err) {
+      done(null);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  // use built-in mongoose method to create multiple instances from an array of objects
+  // Model.create(docs, options, callback)
+  Person.create(arrayOfPeople, (err, data) => {
+    if(err) {
+      done(null)
+    } else {
+      done(null, data)
+    }
+  });
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  // Model.find(query, callback) accepts a query document (a JSON object) and a callback, 
+  // then returns an array of matches.
+  // In search methods, the callback causes the query to execute, otherwise it will just
+  // return the query
+  let query = {name: personName};
+  Person.find(query, (err, data) => {
+    if(err) {
+      done(null)
+    } else {
+      done(null, data)
+    }
+  });
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  let query = {favoriteFoods: food};
+  Person.findOne(query, (err, data) => {
+    if(err) done(null)
+    else done(null, data)
+  });
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if(err) done(null)
+    else done(null, data)
+  });
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    if(err) {
+      done(null)
+    }
+    else {  
+      person.favoriteFoods.push(foodToAdd);
+      person.save((err, data) => {
+        if(err) done(null)
+        else done(null, data)
+      });
+    }
+  });
 };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  
+  const query = {name: personName};
+  const update = {age: ageToSet};
+  const options = {new: true};
+  
+  Person.findOneAndUpdate(query, update, options, (err, data) => {
+    if(err) done(null)
+    else done(null, data)
+  });
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, data) => {
+    if(err) done(null)
+    else done(null, data)
+  });
+
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  const conditions = {name: nameToRemove};
+  Person.deleteMany(conditions, (err, data) => {
+    if(err) done(null)
+    else done(null, data)
+  });
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  // build the query with chaining ending with .exec(callback) to execute the whole thing
+  // when querying an array, passing just the element w/o brackets is like doing 'contains'
+  Person.find({favoriteFoods: foodToSearch})
+    .sort({name: 'asc'})
+    .limit(2)
+    .select('-age')
+    .exec((err, data) => {
+    if(err) done(null)
+    else done(null, data)
+  });
 };
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
  */
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
 exports.PersonModel = Person;
